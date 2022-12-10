a827611571da5efc5c73065f4b586bbfc3026b40
Testowy Koleszka <testowykoleszka@zajavka.pl>
2022-12-10 15:41:15 +0100

    Person class modification 5

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -2,7 +2,7 @@ package pl.zajavka.case2;
 
 import java.math.BigDecimal;
 
-record Person(String name, BigDecimal salary) {
+record Person(String name, Long age, BigDecimal salary) {
 
     public BigDecimal howMuchDoYouEarn() {
         return salary;
